59ca7800eb67d79bd36053fe309789887f3d1436
Rupelio <71788342+Rupelio@users.noreply.github.com>
2024-02-15 16:50:34 -0300

    feat: adding modal with about in pokedex

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,14 @@
-
-class Pokemon {
+class Pokemon{
     number;
     name;
     type;
     types = [];
     photo;
+    baseExp;
+    height;
+    weight;
+    abilities = [];
+    description;
+    generation;
+    eggGroups = [];
 }
